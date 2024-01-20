40a050bc45b09f5591935f33e2b821a249c57a0d
radha M <radhamlakshmi@gmail.com>
2024-01-20 17:11:34 +0530

    first commit

---
+++ src/components/dashboard/redux/dashboard-state.js
@@ -0,0 +1,14 @@
+export const dashboardState = {
+    users: {
+        "rama@gmail.com": {
+            enrolledCourses: []
+        },
+        "karki@cdac.in": {
+            enrolledCourses: []
+        },
+        "radha@outlook.com": {
+            enrolledCourses: []
+        }
+    },
+    currentUser: "radha@gmail.com"
+}
